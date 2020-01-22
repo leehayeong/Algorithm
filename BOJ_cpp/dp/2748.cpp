af359b86941d0e9b73ced43b71f57e356c064d27
leehayeong <lhaayyy@naver.com>
2020-01-22 17:35:01 +0900

    BOJ2748 피보나치 수 2 : dp사용

---
+++ BOJ_cpp/dp/2748.cpp
@@ -0,0 +1,25 @@
+﻿#include <cstdio>
+#include <iostream> 
+#include <algorithm> 
+#include <vector> 
+using namespace std;
+
+long long d[100];
+
+long long fib(long long x) {
+	if (x == 0) return 0;
+	if (x == 1) return 1;
+	if (d[x] != 0) return d[x];
+	return d[x] = fib(x - 1) + fib(x - 2);
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	cout << fib(n);
+}
