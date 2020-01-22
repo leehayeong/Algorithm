f42da0b8be0178f9df903c990f1eae0fe7f555ef
leehayeong <lhaayyy@naver.com>
2020-01-22 17:59:51 +0900

    BOJ1003 피보나치 함수: 1과 0이 호출되는 수

---
+++ BOJ_cpp/dp/1003.cpp
@@ -0,0 +1,39 @@
+﻿#include <cstdio>
+#include <iostream> 
+#include <algorithm> 
+#include <vector> 
+using namespace std;
+
+/*
+피보나치를 구현했을 때, 0과 1이 호출되는 수
+*/
+
+long long d[41][2];
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int t, n;
+	
+	// 0일 때 0의 개수는 1, 1의 개수는 0
+	d[0][0] = 1;
+	d[0][1] = 0;
+
+	// 1일 때 0의 개수는 0, 1의 개수는 1
+	d[1][0] = 0;
+	d[1][1] = 1;
+
+	cin >> t;
+	while (t--) {
+		cin >> n;
+
+		for (int i = 2; i <= n; i++) {
+			d[i][0] = d[i - 1][0] + d[i - 2][0];
+			d[i][1] = d[i - 1][1] + d[i - 2][1];
+		}
+		
+		cout << d[n][0] << " " << d[n][1] << "\n";
+	}
+}
