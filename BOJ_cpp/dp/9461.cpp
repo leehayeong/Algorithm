1e32ea7f31ab5e87ecb222cfa001f663751a8adb
leehayeong <lhaayyy@naver.com>
2020-01-29 16:57:52 +0900

    BOJ9461 파도반수열

---
+++ BOJ_cpp/dp/9461.cpp
@@ -0,0 +1,31 @@
+﻿#include <cstdio>
+#include <iostream> 
+#include <algorithm> 
+#include <vector> 
+using namespace std;
+
+/*
+점화식
+d[n] = d[n-1] + d[n-5]
+*/
+
+long long d[101];
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	long long t, n;
+
+	d[0] = 0, d[1] = 1, d[2] = 1, d[3] = 1, d[4] = 2;
+	for (long long i = 5; i <= 100; i++) {
+		d[i] = d[i - 1] + d[i - 5];
+	}
+
+	cin >> t;
+	while (t--) {
+		cin >> n;
+		cout << d[n] << "\n";
+	}
+}
