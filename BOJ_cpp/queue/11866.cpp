ac29a39ea480640c01e227c59c383ed116472af1
leehayeong <lhaayyy@naver.com>
2020-02-11 17:03:16 +0900

    BOJ11866 요세푸스 문제 0

---
+++ BOJ_cpp/queue/11866.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream> 
+#include <queue>
+using namespace std;
+
+int N, K;
+queue<int> q;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		q.push(i);
+	}
+
+	cout << "<";
+	while (q.size() != 1) {
+		for (int i = 1; i < K; i++) {
+			int x = q.front();
+			q.pop();
+			q.push(x);
+		}
+		cout << q.front() << ", ";
+		q.pop();
+	}
+	cout << q.front() << ">";
+	q.pop();
+}
