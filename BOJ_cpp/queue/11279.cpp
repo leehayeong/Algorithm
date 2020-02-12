fd4ad682cd7ae5b0523a4d821f4311c41d8b3457
leehayeong <lhaayyy@naver.com>
2020-02-12 18:38:31 +0900

    BOJ11279 최대 힙

---
+++ BOJ_cpp/queue/11279.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream> 
+#include <queue>
+using namespace std;
+
+/*
+최대 힙
+배열에 자연수 x를 넣는다.
+배열에서 가장 큰 값을 출력하고, 그 값을 배열에서 제거한다.
+*/
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	priority_queue<int, vector<int>, less<int>> q;
+
+	while (N--) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			if (q.empty()) {
+				cout << 0 << "\n";
+				continue;
+			}
+			cout << q.top() << "\n";
+			q.pop();
+		}
+		else {
+			q.push(x);
+		}
+	}
+}
