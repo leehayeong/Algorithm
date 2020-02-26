cf15fb14659c69e2bf864ff7462151b1ee1a37b1
leehayeong <lhaayyy@naver.com>
2020-02-26 22:34:51 +0900

    BOJ9372 상근이의 여행

---
+++ BOJ_cpp/mst/9372.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> 
+
+using namespace std;
+
+/*
+상근이의 여행
+비행 스케줄이 주어졌을 때, 모든 도시를 여행하기 위해 타야하는 비행기 종류의 최소 개수?
+*/
+
+int N, M;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	while (T--) {
+		cin >> N >> M;
+		for (int i = 0; i < M; i++) {
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << N - 1 << "\n";
+	}
+}
