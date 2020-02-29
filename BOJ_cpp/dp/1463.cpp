5a0ed4fa336c1f9ed4533f570680c9973a1ec75c
leehayeong <lhaayyy@naver.com>
2020-02-29 18:07:08 +0900

    BOJ1463 1로 만들기

---
+++ BOJ_cpp/dp/1463.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> 
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+/*
+1로 만들기
+*/
+
+int N, dp[1000001];
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+
+	for (int i = 2; i <= N; i++) {
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0) dp[i] = min(dp[i], dp[i / 3] + 1);
+		if (i % 2 == 0) dp[i] = min(dp[i], dp[i / 2] + 1);
+	}
+
+	cout << dp[N];
+}
