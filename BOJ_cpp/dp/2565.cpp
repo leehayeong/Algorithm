46d3fd3ebf8ed389b986e15c0dbe0f1c1a71b8b8
leehayeong <lhaayyy@naver.com>
2020-02-29 22:08:38 +0900

    BOJ2565 전깃줄

---
+++ BOJ_cpp/dp/2565.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream> 
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+/*
+전깃줄
+시작 위치로 정렬하고, 도착 위치가 증가하는 부분 수열이 아닌 만큼 없앤다.
+*/
+
+int N, dp[101];
+vector<pair<int, int>> v;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a, b;
+		cin >> a >> b;
+		v.push_back(make_pair(a, b));
+	}
+
+	sort(v.begin(), v.end());	// pair의 first로 정렬 됨
+
+	// 증가하는 부분수열 구하기
+	int length = 0;
+	for (int i = 0; i < N; i++) {
+		dp[i] = 1;
+		for (int j = 0; j < N; j++) {
+			if (v[j].second < v[i].second && dp[j] + 1 > dp[i]) {
+				dp[i] = dp[j] + 1;
+			}
+		}
+		length = max(length, dp[i]);
+	}
+
+	cout << N - length;
+}
