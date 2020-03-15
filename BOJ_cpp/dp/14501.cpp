7a0ddfbaddad941232c8517853e1100ebbe93b1f
leehayeong <lhaayyy@naver.com>
2020-03-15 14:08:55 +0900

    BOJ14501 퇴사

---
+++ BOJ_cpp/dp/14501.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream> 
+#include <algorithm>
+#include <string.h>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+/*
+퇴사
+백준이가 퇴사할 때까지 얻을 수 있는 최대 수익은?
+*/
+
+int N;
+int time[16], pay[16];
+int dp[17];
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cin >> time[i] >> pay[i];
+	}
+
+	for (int i = 1; i <= N + 1; i++) {
+		int finish = i + time[i];
+
+		if (finish <= N + 1) {
+			dp[finish] = max(dp[finish], dp[i] + pay[i]);
+		}
+
+		dp[i + 1] = max(dp[i + 1], dp[i]);
+	}
+
+	cout << dp[N + 1];
+}
