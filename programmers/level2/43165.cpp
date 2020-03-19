797f32a2862850344f3d2e574b53ab9cec8cad06
leehayeong <lhaayyy@naver.com>
2020-03-19 18:52:43 +0900

    programmers 타겟 넘버

---
+++ programmers/level2/43165.cpp
@@ -0,0 +1,29 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int nsize;
+int ans = 0;
+
+void dfs(int idx, int sum, vector<int> numbers, int target) {
+	// n개의 숫자 계산 끝나면 타겟과 넘버와 같은지 비교
+	if (idx == nsize) {
+		if (sum == target) ans++;
+		return;
+	}
+
+	dfs(idx + 1, sum + numbers[idx], numbers, target);
+	dfs(idx + 1, sum - numbers[idx], numbers, target);
+}
+
+int solution(vector<int> numbers, int target) {
+	int answer = 0;
+
+	nsize = numbers.size();
+	dfs(0, 0, numbers, target);
+	answer = ans;
+
+	return answer;
+}
