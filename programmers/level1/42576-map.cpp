9cabb073a5e5b04aaa2d0c7f45b48f049b5415cd
leehayeong <lhaayyy@naver.com>
2020-03-20 22:56:24 +0900

    programmers 완주하지 못한 선수

---
+++ programmers/level1/42576-map.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    
+    map<string, int> m;
+    
+    // 참가자 ++, 완주자 -- 
+    for(int i = 0; i < participant.size(); i++){
+        m[participant[i]]++;
+    }
+    for(int i = 0; i < completion.size(); i++){
+        m[completion[i]]--;
+    }
+
+    // 결과 확인 => 참가하고 완주한 사람은 결과 = 0, 그렇지 않은 사람은 != 0
+    for(int i = 0; i < participant.size(); i++){
+        if(m[participant[i]] != 0){
+            answer = participant[i];
+            break;
+        }
+    }
+    
+    return answer;
+}
---
+++ programmers/level1/42576-sort.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+
+    // 이름 순 정렬
+    sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
+    
+    // 이름이 같지 않을 때가 완주하지 못한 선수가 나올 때
+    for(int i = 0; i < participant.size(); i++){
+        // 참가자의 마지막 순서까지 반복문이 왔다면, 마지막 참가자가 완주하지 못한 선수임
+        if(i == participant.size() - 1){
+            answer = participant[i];
+            break;
+        }
+        
+        if(participant[i] != completion[i]){
+            answer = participant[i];
+            break;
+        }
+    }
+    
+    return answer;
+}
