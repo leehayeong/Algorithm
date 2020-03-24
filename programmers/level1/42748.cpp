b94dc12ce2125d5be50de48a7e382e48044ce8b5
leehayeong <lhaayyy@naver.com>
2020-03-24 16:05:43 +0900

    programmers K번째수

---
+++ programmers/level1/42748.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+/*
+k번째 수
+i번째부터 j번째까지 자른 후 k번째 찾기
+*/
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+	vector<int> answer;
+    
+    for(int i = 0; i < commands.size(); i++){
+        vector<int> tmp;
+        for(int j = commands[i][0] - 1; j <= commands[i][1] - 1; j++){
+            tmp.push_back(array[j]);                // i번째부터 j번째까지 push
+        }
+        sort(tmp.begin(), tmp.end());               // 정렬
+        answer.push_back(tmp[commands[i][2] - 1]);  // k번째 수 저장
+    }
+    
+	return answer;
+}
