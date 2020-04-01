4219a6aac19d42883f7a7e55ec2981bdbfd5f8ab
leehayeong <lhaayyy@naver.com>
2020-04-01 17:27:18 +0900

    programmers 저울

---
+++ programmers/level3/42886.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+/*
+저울
+추들로 측정할 수 없는 무게의 최솟값?
+*/
+
+int solution(vector<int> weight) {
+    int answer = 1;
+
+    sort(weight.begin(), weight.end());                 // 1. 정렬하고
+    
+    for(int i = 0; i < weight.size(); i++){             // 2. 더해가다가, 추의 값이 더 커질 때 종료
+        if(answer >= weight[i]) answer += weight[i];
+        else break;
+    }
+    
+    return answer;
+}
