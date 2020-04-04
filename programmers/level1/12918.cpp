2fec57c77b9d799505aad92badfd9e357063fddf
leehayeong <lhaayyy@naver.com>
2020-04-04 16:13:08 +0900

    programmers 문자열 다루기 기본

---
+++ programmers/level1/12918.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+문자열 다루기 기본
+문자열의 길이가 4 혹은 6이고, 숫자로만 이루어져있으면 true
+*/
+
+bool solution(string s) {
+    int length = s.length();
+    
+    if(length != 4 && length != 6) return false;
+    for(int i = 0; i < s.length(); i++){
+        if(!('0' <= s[i] && s[i] <= '9')) return false;
+    }
+    
+    return true;
+}
