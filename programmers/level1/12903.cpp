a9ba2a62f68816d55d95c2cff23538a9719142a4
leehayeong <lhaayyy@naver.com>
2020-04-04 15:31:40 +0900

    programmers 가운데 글자 가져오기

---
+++ programmers/level1/12903.cpp
@@ -0,0 +1,22 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+가운데 글자 가져오기
+단어의 가운데 글자 반환 (짝수는 두 글자)
+*/
+
+string solution(string s) {
+    string answer = "";
+    int length = s.length();
+    
+    if(length % 2 == 0){
+        answer = s.substr(length / 2 - 1, 2);
+    } else {
+        answer = s[length / 2];
+    }
+    
+    return answer;
+}
