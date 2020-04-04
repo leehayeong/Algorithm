072a34dfdf495de90d1eb66c14d3e4d79b11bcc4
leehayeong <lhaayyy@naver.com>
2020-04-04 19:09:07 +0900

    programmers 자연수 뒤집어 배열로 만들기

---
+++ programmers/level1/12932.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+자연수 뒤집어 배열로 만들기
+*/
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    
+    int i = 10;
+    while(n >= 1){
+        answer.push_back(n % i);
+        n = n / i;
+    }
+    
+    return answer;
+}
