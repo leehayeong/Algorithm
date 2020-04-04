fb6f3aab9066d324aad8eb76f7f023c283fa3f18
leehayeong <lhaayyy@naver.com>
2020-04-04 19:31:01 +0900

    programmers 정수 제곱근 판별

---
+++ programmers/level1/12934.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/*
+정수 제곱근 판별
+x^2 = n 이라면 (x+1)^2, 아니면 -1 리턴
+*/
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    if(sqrt(n) - (int)sqrt(n) == 0) answer = pow(sqrt(n) + 1, 2);   // 소수점 있는지 판별
+    else answer = -1;
+    
+    return answer;
+}
