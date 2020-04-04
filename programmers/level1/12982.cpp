0069f406d7e1861983124e8c3f6ff57c70b24333
leehayeong <lhaayyy@naver.com>
2020-04-05 00:14:51 +0900

    programmers 예산 (level1)

---
+++ programmers/level1/12982.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+예산
+최대 몇 개의 부서에 물품을 지원 가능?
+*/
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(), d.end());               // 금액 적은 순으로 정렬하고
+    
+    int sum = 0;
+    for(int i = 0; i < d.size(); i++){      // 예산안 넘어가면 stop
+        if(sum + d[i] > budget) break;
+        sum += d[i];
+        answer++;
+    }
+    
+    return answer;
+}
