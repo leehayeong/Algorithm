eaf3d90924283995b9d1c6fcf09e392993c34bd0
leehayeong <lhaayyy@naver.com>
2020-04-04 16:17:43 +0900

    programmers 서울에서 김서방 찾기

---
+++ programmers/level1/12919.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+서울에서 김서방 찾기
+Kim이 있는 index 반환
+*/
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    
+    for(int i = 0; i < seoul.size(); i++){
+        if(seoul[i] == "Kim"){
+            answer = "김서방은 " + to_string(i) + "에 있다";
+            break;
+        }
+    }
+    
+    return answer;
+}
