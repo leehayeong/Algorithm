f56e05858d5ac39f2257ce5f71fb630243075d59
leehayeong <lhaayyy@naver.com>
2020-04-04 16:12:30 +0900

    programmers 문자열 내림차순으로 배치하기

---
+++ programmers/level1/12917.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+문자열 내림차순으로 배치하기
+*/
+
+string solution(string s) {
+    string answer = "";
+    sort(s.begin(), s.end(), greater<int>());
+    answer = s;
+    return answer;
+}
