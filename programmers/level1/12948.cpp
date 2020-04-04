61b938c9fb71fc7a2ba4d3009ecdab9fd50bd9c5
leehayeong <lhaayyy@naver.com>
2020-04-04 21:28:29 +0900

    programmers 핸드폰 번호 가리기

---
+++ programmers/level1/12948.cpp
@@ -0,0 +1,20 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/*
+핸드폰 번호 가리기
+*/
+
+string solution(string phone_number) {
+    string answer = "";
+    int length = phone_number.length();
+    int hide = length - 4;
+    
+    for(int i = 0; i < hide; i++) answer += "*";
+    answer += phone_number.substr(hide);
+    
+    return answer;
+}
