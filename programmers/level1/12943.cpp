b0edfaf7b996c10798111d8932236c61f0ac238c
leehayeong <lhaayyy@naver.com>
2020-04-04 21:18:22 +0900

    programmers 콜라츠 추측

---
+++ programmers/level1/12943.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/*
+콜라츠 추측
+1. num 짝수면 2로 나눔 / 홀수면 3을 곱하고 1을 더함
+2. 1이 될 때까지 반복
+*/
+
+int solution(int num) {
+    int answer = 0;
+    
+    while(answer <= 500){
+        if(num == 1) break;
+
+        if(num % 2 == 0) num = num / 2;
+        else if(num % 2 == 1) num = num * 3 + 1;
+        answer++;
+    }
+    
+    if(answer > 500) answer = -1;
+    
+    return answer;
+}
