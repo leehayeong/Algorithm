7f77ef655399ac433654559cfd42957486c5cab4
leehayeong <lhaayyy@naver.com>
2020-04-04 19:02:12 +0900

    programmers 자릿수 더하기

---
+++ programmers/level1/12931.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+자릿수 더하기
+*/
+
+int solution(int n)
+{
+    int answer = 0;
+    
+    int i = 10;
+    while(n >= 1){
+        answer += n % i;
+        n = n / i;
+    }
+
+    return answer;
+}
