110a53c6e4afc39568a61b299a8cfee231f5db90
leehayeong <lhaayyy@naver.com>
2020-04-04 16:06:29 +0900

    programmers 문자열 내 p와 y의 개수

---
+++ programmers/level1/12916.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+/*
+문자열 내 p와 y의 개수
+p개수 = y개수이면 true, 아니면 false
+*/
+
+bool solution(string s)
+{
+    bool answer = true;
+    int pcnt = 0, ycnt = 0;
+    
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == 'Y' || s[i] == 'y') ycnt ++;
+        else if(s[i] == 'P' || s[i] == 'p') pcnt++;
+    }
+    
+    if(ycnt != pcnt) answer = false;
+    
+    return answer;
+}
