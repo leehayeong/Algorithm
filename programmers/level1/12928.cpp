5a8046a0dd9c5959fe01d9eb34561f71f8707a7c
leehayeong <lhaayyy@naver.com>
2020-04-04 16:58:00 +0900

    programmers 약수의 합

---
+++ programmers/level1/12928.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+약수의 합
+n의 약수를 모두 더한 합을 리턴
+*/
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 1; i <= n; i++){
+        if(n % i == 0) answer += i;
+    }
+    
+    return answer;
+}
