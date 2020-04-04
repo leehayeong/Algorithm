aa9871af5ec843987152a399fa05a8205a76ff75
leehayeong <lhaayyy@naver.com>
2020-04-04 16:56:06 +0900

    programmers 시저 암호

---
+++ programmers/level1/12926.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/*
+시저 암호
+알파벳을 n만큼 민 문자열
+*/
+
+string solution(string s, int n) {
+    string answer = "";
+    
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == ' '){
+            answer += " ";
+            continue;
+        }
+        
+        int next = s[i] + n;
+        if((s[i] <= 'z' && next > 'z') || (s[i] <= 'Z' && next > 'Z')) next -= 26;  // 범위 벗어나면 앞으로 돌림
+        answer += (char)next;
+    }
+    
+    return answer;
+}
