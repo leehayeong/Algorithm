9f2a5abd18d61eb97ac637a09222ec11529d9cef
leehayeong <lhaayyy@naver.com>
2020-04-04 16:38:01 +0900

    programmers 수박수박수박수박수박수?

---
+++ programmers/level1/12922.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/*
+수박수박수박수박수박수?
+수박 반복하여 길이 n인 string 만들기
+*/
+
+string solution(int n) {
+    string answer = "";
+    
+    if(n % 2 == 0){
+        for(int i = 1; i <= n; i += 2) answer += "수박";
+    } else {
+        for(int i = 1; i < n; i += 2) answer += "수박";
+        answer += "수";
+    }
+    
+    return answer;
+}
