b9819a555601da2cd81dcd15774e004259e2523e
leehayeong <lhaayyy@naver.com>
2020-04-04 19:15:44 +0900

    programmers 정수 내림차순으로 배치하기

---
+++ programmers/level1/12933.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+/*
+정수 내림차순으로 배치하기
+스트링 이용해 정렬
+*/
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    string num = to_string(n);
+    sort(num.begin(), num.end(), greater<int>());
+    answer = stoll(num);    // long long으로의 변환은 stoi가 아닌 stoll
+    
+    return answer;
+}
