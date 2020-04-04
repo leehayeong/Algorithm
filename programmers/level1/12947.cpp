7628c76daa43eee4e9adec6c97d450c0f7d276d7
leehayeong <lhaayyy@naver.com>
2020-04-04 21:24:15 +0900

    programmers 하샤드 수

---
+++ programmers/level1/12947.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+하샤드 수
+x자릿수의 합으로 x가 나누어지면 하샤드수 (x % x자릿수 합 == 0)
+*/
+
+bool solution(int x) {
+    bool answer = true;
+    
+    int i = 10;
+    int n = x;
+    int sum = 0;
+    while(n >= 1){
+        sum += n % i;
+        n = n / i;
+    }
+    
+    if(x % sum != 0) answer = false;
+    
+    return answer;
+}
