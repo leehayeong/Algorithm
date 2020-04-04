430b9c3d31daca2a110b96ef0f8d0680f4c755e5
leehayeong <lhaayyy@naver.com>
2020-04-04 21:21:10 +0900

    programmers 평균 구하기

---
+++ programmers/level1/12944.cpp
@@ -0,0 +1,18 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+평균 구하기
+*/
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    int n = arr.size();
+    
+    for(int i = 0; i < n; i++) answer += arr[i];
+    answer = answer / n;
+    
+    return answer;
+}
