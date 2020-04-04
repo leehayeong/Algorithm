4903c0c3d69d14e43fe9f72b27eabe5f1c626ddd
leehayeong <lhaayyy@naver.com>
2020-04-04 21:39:36 +0900

    programmers 직사각형 별찍기

---
+++ programmers/level1/12969.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/*
+직사각형 별찍기
+a, b 입력받아 b*a인 직사각형 찍기
+*/
+
+int main(void) {
+    int a, b;
+    
+    cin >> a >> b;
+    for(int i = 0; i < b; i++){
+        for(int j = 0; j < a; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
