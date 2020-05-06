9f9d4b9d0fc843dbb4b7d300837594ba1a9b03fc
leehayeong <lhaayyy@naver.com>
2020-05-06 15:26:41 +0900

    SWEA1204 최빈수 구하기

---
+++ swea/d3/1204.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+/*
+최빈수 구하기
+*/
+
+int main(int argc, char** argv)
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int test_case;
+	int T;
+	cin >> T;
+	while (T--) {
+		cin >> test_case;
+
+		int cnt[101] = { 0, };
+		int maxCnt = 0, maxScore = 0;
+
+		for (int i = 0; i < 1000; i++) {
+			int score;
+			cin >> score;
+
+			// 점수 횟수 기록
+			cnt[score]++;
+
+			// 최빈값 찾기
+			if (maxCnt < cnt[score]) {
+				maxCnt = cnt[score];
+				maxScore = score;
+			}
+			else if (maxCnt == cnt[score] && maxScore < score) {
+				maxScore = score;
+			}
+		}
+
+		cout << "#" << test_case << " " << maxScore << "\n";
+	}
+
+	return 0;
+}
