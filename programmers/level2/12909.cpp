d78d6cb423d3cd31a0949cc6a48994ee875c5bd1
leehayeong <lhaayyy@naver.com>
2020-05-15 23:48:33 +0900

    programmers 올바른 괄호

---
+++ programmers/level2/12909.cpp
@@ -0,0 +1,31 @@
+﻿#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+// 올바른 괄호
+
+bool solution(string s)
+{
+	bool answer = true;
+	int length = s.length();
+	stack<char> st;
+
+	for (int i = 0; i < length; i++) {
+		if (s[i] == '(') {
+			st.push(s[i]);
+		}
+		else {
+			if (st.empty()) {
+				answer = false;
+				break;
+			}
+			st.pop();
+		}
+	}
+
+	if (!st.empty()) answer = false;
+
+	return answer;
+}
