b793fa2e611954e06981d56294f1de907fd10888
leehayeong <lhaayyy@naver.com>
2020-05-15 21:12:46 +0900

    programmers 최댓값과 최솟값

---
+++ programmers/level2/12939.cpp
@@ -0,0 +1,27 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+// 최댓값과 최솟값
+
+string solution(string s) {
+	string answer = "";
+	vector<int> v;
+	int start = 0, end = 0;
+
+	// 띄어쓰기로 문자열 파싱
+	while (end != -1) {
+		end = s.find(' ', start);
+		v.push_back(atoi(s.substr(start, end - start).c_str()));
+		start = end + 1;
+	}
+
+	// 정렬
+	sort(v.begin(), v.end());
+	answer = to_string(v[0]) + " " + to_string(v[v.size() - 1]);
+
+	return answer;
+}
