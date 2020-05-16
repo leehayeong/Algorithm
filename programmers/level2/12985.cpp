442bce796459e4e2f33a0d500ee1c02a15f7bca4
leehayeong <lhaayyy@naver.com>
2020-05-17 00:32:14 +0900

    programmers 예상 대진표

---
+++ programmers/level2/12985.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// 예상 대진표
+
+int solution(int n, int a, int b)
+{
+	int answer = 0;
+
+	// 두 사람의 index가 같아졌을 때 대전이 끝나고 승부가 난 것
+	while (a != b) {
+		// 홀수는 / 2 + 1 인덱스로, 짝수는 / 2 인덱스로 바뀜 (=반올림)
+		a = (a + 1) / 2;
+		b = (b + 1) / 2;
+		answer++;
+	}
+
+	return answer;
+}
