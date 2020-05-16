1e0b52a276ae82202f8c188df116aab8dd15801a
leehayeong <lhaayyy@naver.com>
2020-05-16 16:26:15 +0900

    programmers 땅따먹기

---
+++ programmers/level2/12913.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+	int answer = 0;
+	vector<pair<int, int>> tmp;
+
+	for (int i = 0; i < land.size() - 1; i++) {
+		for (int j = 0; j < 4; j++) {
+			tmp.push_back(make_pair(land[i][j], j));
+		}
+
+		sort(tmp.rbegin(), tmp.rend());
+
+		int max = tmp[0].first;
+		int idx = tmp[0].second;
+		for (int k = 0; k < 4; k++) {
+			if (idx == k) {
+				max = tmp[1].first;
+				idx = tmp[1].second;
+			}
+
+			land[i + 1][k] += max;
+
+			max = tmp[0].first;
+			idx = tmp[0].second;
+		}
+
+		tmp.clear();
+	}
+
+	for (int i = 0; i < 4; i++) {
+		answer = max(answer, land[land.size() - 1][i]);
+	}
+
+	return answer;
+}
